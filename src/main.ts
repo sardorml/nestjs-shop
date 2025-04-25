77a64fc63c0c72570c13f2c276b8919b1d6f6a60
sardorml <sardormamadaliyev@gmail.com>
2025-04-25 17:00:45 +0500

    setup swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Shop Inventory API')
+    .setDescription('API for managing catalogs and products')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
